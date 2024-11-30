c06b186fb26d6ea32ea169e45c39344aa438ae8b
Gorocy <jakubgoracy@student.agh.edu.pl>
2024-11-30 10:17:30 +0100

    rpc

--- rpc.js
+++ rpc.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 let auth;
 
 if (process.env.RPCCOOKIE) {
@@ -10,16 +10,44 @@ if (process.env.RPCCOOKIE) {
 }
 
 if (process.env.RPCAUTH) {
-    auth = process.env.RPCAUTH;
+  auth = process.env.RPCAUTH;
 }
 
 if (!auth) {
   console.log(`RPCAUTH or RPCCOOKIE must be specified`);
 }
+if (!process.env.RPCUSER) {
+  console.log(`RPCUSER is deprecated)`);
+} else {
+  console.log(process.env.RPCUSER);
+}
+if (!process.env.PASSWORD) {
+  console.log(`PASSWORD is deprecated)`);
+} else {
+  console.log(process.env.PASSWORD);
+}
 
-module.exports = require('yajrpc/qup')({
-  url: process.env.RPC || 'http://localhost:8332',
+if (!process.env.RPC) {
+  console.log(`RPC is deprecated)`);
+} else {
+  console.log(process.env.RPC);
+}
+
+if (!process.env.RPCBATCHSIZE) {
+  console.log(`RPCBATCHSIZE is deprecated)`);
+} else {
+  console.log(process.env.RPCBATCHSIZE);
+}
+
+if (!process.env.RPCCONCURRENT) {
+  console.log(`RPCCONCURRENT is deprecated)`);
+} else {
+  console.log(process.env.RPCCONCURRENT);
+}
+
+module.exports = require("yajrpc/qup")({
+  url: process.env.RPC || "http://localhost:8332",
   auth: auth,
   batch: process.env.RPCBATCHSIZE || 500,
-  concurrent: process.env.RPCCONCURRENT || 16
-})
+  concurrent: process.env.RPCCONCURRENT || 16,
+});
